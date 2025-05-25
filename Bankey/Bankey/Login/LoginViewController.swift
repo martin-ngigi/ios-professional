d68b12d9ff6b74ca69451f4614afdb7bc42965ff
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-05-25 10:30:36 +0300

    Added Login Button

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let  loginView = LoginView()
+    let signInButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +25,19 @@ class LoginViewController: UIViewController {
 
 extension LoginViewController {
     func style() {
-        loginView.translatesAutoresizingMaskIntoConstraints = false //
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // SignIn Button
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8
+        signInButton.setTitle("Sign In", for: [])
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
     }
     
     func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
         
         // LoginView
         NSLayoutConstraint.activate([ // Sets isActive tot true for all contraints contained within.
@@ -36,6 +45,20 @@ extension LoginViewController {
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 2),
             view.centerYAnchor.constraint(equalTo: loginView.centerYAnchor), // center vertically
         ])
+        
+        // Button
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
                
+    }
+}
+
+//MARK: Actions
+extension LoginViewController{
+    @objc func signInTapped() {
+        print("Sign In Tapped")
     }
 }
