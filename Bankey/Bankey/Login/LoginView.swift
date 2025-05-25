6d1adb447558544b61fc5cbac30a886fac1a3c03
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-05-25 09:56:26 +0300

    Added Username textfield

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class LoginView: UIView {
     
-    //let usernameTextField = UITextField()
+    let usernameTextField = UITextField()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,9 +33,43 @@ extension  LoginView {
     func style(){
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
+        
+        // Username
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self // Textfield is going to send us messages through protocol
     }
     
     func layout() {
+        
+        addSubview(usernameTextField)
+        
+        // Username
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1), // padding top
+            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1), // padding leading
+            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1) // padding trailing
+        ])
+    }
+}
+
+//MARK: UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
 }
