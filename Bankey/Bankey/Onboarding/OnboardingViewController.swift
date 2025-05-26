34d86c6e71b6b65dbd5c523bc6c362c88a5a1a48
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-05-26 09:04:17 +0300

    added generic onboarding controller

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -13,6 +13,20 @@ class OnboardingViewController: UIViewController{
     let imagaeView =  UIImageView ()
     let label = UILabel()
     
+    let heroImageName: String
+    let titleText: String
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
@@ -23,6 +37,8 @@ class OnboardingViewController: UIViewController{
 extension OnboardingViewController{
     
     func style(){
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -30,7 +46,7 @@ extension OnboardingViewController{
         //Image View
         imagaeView.translatesAutoresizingMaskIntoConstraints = false
         imagaeView.contentMode = .scaleAspectFit
-        imagaeView.image = UIImage(named: "apple")
+        imagaeView.image = UIImage(named: heroImageName)
         
         //Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -38,7 +54,7 @@ extension OnboardingViewController{
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you fell like you are back in 1989."
+        label.text = titleText
     }
     
     func layout(){
