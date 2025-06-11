0c98cc333dd3b0423688860a17b8a7864bc935c3
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-06-11 10:07:22 +0300

    Logout notification Center

---
+++ Bankey/Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Hummingbird on 11/06/2025.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
