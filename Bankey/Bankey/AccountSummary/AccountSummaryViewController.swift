7e5f95ca8897bf2c05ace98123750a6561bf9f20
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-07-02 08:20:09 +0300

    Load API Accounts and profile

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 class AccountSummaryViewController: UIViewController {
     
     var profile: Profile?
+    var accounts: [Account] = []
+    
     var headerViewmodel = AccountSummaryHeaderView.ViewModel(welcomeText: "Welcome", name: "", date: Date())
     
     //let games = [ "Pacman", "Space Invaders", "Space Patrol"]
@@ -100,35 +102,6 @@ extension AccountSummaryViewController: UITableViewDelegate {
     }
 }
 
-extension AccountSummaryViewController {
-    private func fetchAccounts() {
-        let savings = AccountSummaryCell.ViewModel(accountType: .Banking,
-                                                            accountName: "Basic Savings",
-                                                        balance: 929466.23)
-        let chequing = AccountSummaryCell.ViewModel(accountType: .Banking,
-                                                    accountName: "No-Fee All-In Chequing",
-                                                    balance: 17562.44)
-        let visa = AccountSummaryCell.ViewModel(accountType: .CreditCard,
-                                                       accountName: "Visa Avion Card",
-                                                       balance: 412.83)
-        let masterCard = AccountSummaryCell.ViewModel(accountType: .CreditCard,
-                                                       accountName: "Student Mastercard",
-                                                       balance: 50.83)
-        let investment1 = AccountSummaryCell.ViewModel(accountType: .Investment,
-                                                       accountName: "Tax-Free Saver",
-                                                       balance: 2000.00)
-        let investment2 = AccountSummaryCell.ViewModel(accountType: .Investment,
-                                                       accountName: "Growth Fund",
-                                                       balance: 15000.00)
-
-        accountSummaryCell.append(savings)
-        accountSummaryCell.append(chequing)
-        accountSummaryCell.append(visa)
-        accountSummaryCell.append(masterCard)
-        accountSummaryCell.append(investment1)
-        accountSummaryCell.append(investment2)
-    }
-}
 
 // MARK: Actions
 extension AccountSummaryViewController {
@@ -153,7 +126,16 @@ extension AccountSummaryViewController {
             }
         }
 
-        fetchAccounts()
+        fetchAccounts(forUserId: "1") { result in
+            switch result {
+            case .success(let accounts):
+                self.accounts = accounts
+                self.configureTableCells(with: accounts)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureTableHeaderView(with profile: Profile) {
@@ -162,4 +144,12 @@ extension AccountSummaryViewController {
                                                     date: Date())
         headerView.configure(viewModel: vm)
     }
+    
+    private func configureTableCells(with accounts: [Account]) {
+        accountSummaryCell = accounts.map {
+                AccountSummaryCell.ViewModel(accountType: $0.type,
+                                             accountName: $0.name,
+                                             balance: $0.amount)
+            }
+        }
 }
