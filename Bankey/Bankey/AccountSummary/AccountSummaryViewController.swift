75f703172724351addef48e3ac95093deb669ede
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-07-03 08:33:34 +0300

    Error Handling

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -135,7 +135,8 @@ extension AccountSummaryViewController {
             case .success(let profile):
                 self.profile = profile
             case .failure(let error):
-                print(error.localizedDescription)
+                print("DEBUG: error occured while fetching profile: \(error)")
+                self.displayError(error)
             }
             group.leave()
         }
@@ -146,7 +147,8 @@ extension AccountSummaryViewController {
             case .success(let accounts):
                 self.accounts = accounts
             case .failure(let error):
-                print(error.localizedDescription)
+                print("DEBUG: error occured while fetching accounts: \(error)")
+                self.displayError(error)
             }
             group.leave()
         }
@@ -177,6 +179,31 @@ extension AccountSummaryViewController {
                                              balance: $0.amount)
             }
         }
+    
+    
+    private func displayError(_ error: NetworkError) {
+        let title: String
+        let message: String
+        switch error {
+        case .serverError:
+            title = "Server Error"
+            message = "We could not process your request. Please try again."
+        case .decodingError:
+            title = "Network Error"
+            message = "Ensure you are connected to the internet. Please try again."
+        }
+        self.showErrorAlert(title: title, message: message)
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 // MARK: Actions
